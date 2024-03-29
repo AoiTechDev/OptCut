a4b39ee4677584511fe596d16c5fb55e747faab1
Pawel <pawel.bornikowski@gmail.com>
2024-03-29 02:14:14 +0100

    feat: display amount of stock items, total waste, and total patterns

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,16 @@ export default function Home() {
     []
   );
 
-  console.log(result)
+  function countWaste(result: Array<CuttingPattern & QuantityType>) {
+    let waste = 0;
+    for (let i = 0; i < result.length; i++) {
+      waste += result[i].wasteAmount;
+    }
+    return waste;
+  }
+  console.log(result);
   return (
-    <div className="w-full px-4 min-[1300px]:px-24  mx-auto min-h-screen flex-col  justify-center items-center gap-12">
+    <div className="w-full pb-12 px-4 min-[1300px]:px-24  mx-auto min-h-screen flex-col  justify-center items-center gap-12">
       <div className="flex flex-col min-[1300px]:flex-row justify-center items-center min-[1300px]:items-start h-full gap-12 min-[1300px]:gap-24 min-[1300px]:mt-32">
         <FullTable
           header="Source / Stock Items"
@@ -56,35 +63,97 @@ export default function Home() {
           }
         />
       </div>
-      <div className="w-full max-w-3xl mx-auto mt-32 space-y-4 ">
+      <div className="w-full max-w-3xl mx-auto mt-32 space-y-8 ">
         {result.map((pattern, index) => {
-
           const allSourceItemsLength = stockItems.partItems.map(
             (item) => item.length
           );
-          const amount = pattern.amountOfDemandItem
-            .map((item) => [item.length, item.quantity])
-        
+          const amount = pattern.amountOfDemandItem.map((item) => [
+            item.length,
+            item.quantity,
+          ]);
+
           return (
-            <div key={index} className="w-full bg-black h-10 flex items-start ">
+            <div
+              key={index}
+              className="flex w-full justify-center items-center gap-2 flex-col"
+            >
+              <div className="w-full flex justify-center items-center gap-4 ">
+                <div className="w-24">
+                  {pattern.stockLength} x {pattern.quantity}
+                </div>
+                <div className="w-full bg-gray-700 h-10 flex  text-white font-bold">
+                  {pattern.amountOfDemandItem.map((item, index) => {
+                    return (
+                      <div
+                        className="bg-purple-500"
+                        key={index}
+                        style={{
+                          width: `${Math.ceil(
+                            (item.quantity * item.length * 100) /
+                              pattern.stockLength
+                          )}%`,
+                          height: "100%",
 
-                {pattern.amountOfDemandItem.map((item, index) => {
-
-                 
-                 
-                  return <div key={index}  style={{
-                    width: `${(item.quantity * item.length) * 100 / pattern.stockLength }%`,
-                    height: "100%",
-                    backgroundColor: "red",
-                    display: "inline-block",
-                    border: "1px solid white",
-                  }}><div className=" flex justify-center items-center h-full">{item.length} x {item.quantity}</div></div>
-                })}
-              {/* {pattern.quantity} x {pattern.stockLength}{" "} */}
+                          display: "inline-block",
+                          borderRight: "1px solid white",
+                        }}
+                      >
+                        <div className=" flex justify-center items-center h-full">
+                          {item.length} x {item.quantity}
+                        </div>
+                      </div>
+                    );
+                  })}
+                  {/* {pattern.quantity} x {pattern.stockLength}{" "} */}
+                </div>
+              </div>
+              <div className="w-full text-end">
+                Waste: {pattern.wasteAmount}
+              </div>
             </div>
           );
         })}
       </div>
+
+      {!!result.length ? (
+        <div className="w-full max-w-3xl mx-auto mt-16 space-y-6">
+          <div className="space-y-2">
+            <h4 className="text-xl font-medium">Stock Items Used:</h4>
+            <ul>
+              {result.map((pattern, index) => {
+                return (
+                  <li className="text-lg" key={index}>
+                    {pattern.stockLength} x {pattern.quantity}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="space-y-2">
+            <h4 className="text-xl font-medium">Total Waste Generated:</h4>
+            <span className="text-lg">{countWaste(result)}</span>
+          </div>
+          <div className="space-y-2">
+            <h4 className="text-xl font-medium">Patters Used:</h4>
+            <ul>
+              {result.map((pattern, index) => {
+                return (
+                  <li key={index} className="flex gap-4">
+                    <div className="w-4">{pattern.quantity}</div>
+                    <div className="w-4">x</div>
+                    <div >
+                      {pattern.cutLengths.map((cutLength, index) => (
+                        <span key={index}>[ {cutLength} ] </span>
+                      ))}
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      ) : null}
     </div>
-  );  
+  );
 }
